bee1a3605f28ae8bd1c3f00742d8092c62e010e2
oscarraichert <oscarraichert@hotmail.com>
2024-05-07 13:45:36 -0300

    alterando api de tempo e corrigindo localização

--- lib/services/forecast.service.dart
+++ lib/services/forecast.service.dart
@@ -7,7 +7,7 @@ class ForecastService {
   static const apiUri = 'https://api.open-meteo.com/v1/';
 
   static Future<ForecastModel> getCurrentForecast(double latitude, double longitude) async {
-    var response = await http.get(Uri.parse('${apiUri}forecast?current=temperature_2m,weather_code&latitude=$latitude&longitude=$longitude&timezone=auto'));
+    var response = await http.get(Uri.parse('${apiUri}gfs?current=temperature_2m,weather_code&latitude=$latitude&longitude=$longitude&timezone=auto'));
 
     return ForecastModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   }
